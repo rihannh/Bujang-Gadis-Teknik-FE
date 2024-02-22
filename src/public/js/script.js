126038b9394c387f9b1e1d33a99dbc1fa888062d
Rihan Naufaldihanif <rihannaufal22@gmail.com>
2024-02-22 20:10:41 +0700

    feat:completing voting page

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,18 +1,48 @@
+//mobile-nav-button
+
+const mobileNavButton = document.getElementById('mobile-nav');
+const navMenu = document.getElementById('nav-menu');
+
+mobileNavButton.addEventListener('click', function () {
+    mobileNavButton.classList.toggle('mobile-nav-button-active');
+    navMenu.classList.toggle('hidden');
+});
+
+// navbar 
 window.onscroll = function () {
-    const header = document.querySelector('nav');
-    const ulElement = document.querySelector('#target-ul');
+    const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
-        ulElement.classList.remove('bg-background');
-        ulElement.classList.remove('border-gray-500');
-        ulElement.classList.remove('border');
     } else {
         header.classList.remove('navbar-fixed');
-        ulElement.classList.add('bg-background');
-        ulElement.classList.add('border-gray-500');
-        ulElement.classList.add('border');
+    }
+}
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+const span = document.getElementById("closeModal");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.classList.remove('hidden');
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.classList.add('hidden');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.classList.add('hidden');
     }
 }
 
