f2f1fc0fd0c605073351362d5e5c72ca9b00d1b7
Rihan Naufaldihanif <rihannaufal22@gmail.com>
2024-02-22 17:05:04 +0700

    feat:add voting page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+    "./**/*.{html,js}"],
   theme: {
     container: {
       center: true,
