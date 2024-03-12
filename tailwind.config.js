5368a0e41a737615f007efa943c039250f6aa648
Rihan Naufaldihanif <rihannaufal22@gmail.com>
2024-03-12 09:13:32 +0700

    feat: remake overall

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,34 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: '24px',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      unique: ["Lily Script One", 'system-ui'],
     },
     extend: {
       backgroundImage: {
-        'jumbotron': "url('/src/public/media/banner.webp')",
+        'jumbotron': "url('/src/public/media/banner.png')",
       },
       colors: {
-        primary: '#E6A514',
-        secondary: '#27274A',
-        background: '#151521',
+        dark: '#101820',
+        'gold': {
+          100: '#FFF9E6',
+          200: '#FFE38E',
+          300: '#FFD861',
+          400: '#FFCE35',
+          500: '#EAB308',
+          600: '#C19200',
+          700: '#987300',
+          800: '#705400',
+          900: '#473600',
+        },
       },
       screens: {
         '2xl': '1320px',
       },
     },
   },
-  plugins: [
-  ],
+  plugins: [],
 }
